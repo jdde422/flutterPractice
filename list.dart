6017411a38652ec58a556e77f9291366b373baa4
jdde19 <jdestadilla@up.edu.ph>
2025-02-01 19:56:54 +0800

    first commit

---
+++ list.dart
@@ -0,0 +1,12 @@
+void main ()
+{
+   Object foo = 12;
+   List<Object> scores = [50,75,20];
+   scores[0] = "mario";
+   scores[1] = 12.5;
+   double sc = 12.5;
+   print(scores.indexOf(19));
+
+   var names = {'mario', 'luigi'};
+   Set<Object> namess = {'mario', 12};
+}
