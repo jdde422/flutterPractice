2b302b73b3cab076ba0ade04844e1662454ecb88
jdde19 <jdestadilla@up.edu.ph>
2025-02-01 19:35:02 +0800

    first commit

---
+++ first.dart
@@ -0,0 +1,19 @@
+void main ()
+{
+    // for (var i = 0; i < 5; i++)
+    // {
+    //     print(i);
+    // }
+   
+  //  var foo;
+  //  int? bar;
+  //  print(foo);
+  //  print(bar);
+
+  print(greet("Jd", 21));
+}
+
+String greet(String name, int age)
+{
+  return "hi! my name is $name and i am $age years old";
+}
